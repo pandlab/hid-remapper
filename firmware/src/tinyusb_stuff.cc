0641f3c3896751fefa16217734f7d20175577459
pandlab <pandlab>
2026-09-18 21:56:41 +0200

    CHANGE NAME

--- firmware/src/tinyusb_stuff.cc
+++ firmware/src/tinyusb_stuff.cc
@@ -37,8 +37,8 @@
 
 // These IDs are bogus. If you want to distribute any hardware using this,
 // you will have to get real ones.
-#define USB_VID 0xCAFE
-#define USB_PID 0xBAF2
+#define USB_VID 0x1EA7
+#define USB_PID 0x0066
 
 tusb_desc_device_t desc_device = {
     .bLength = sizeof(tusb_desc_device_t),
@@ -126,14 +126,10 @@ const uint8_t* configuration_descriptors[] = {
 };
 
 char const* string_desc_arr[] = {
-    (const char[]){ 0x09, 0x04 },  // 0: is supported language is English (0x0409)
-#ifdef PICO_RP2350
-    "RP2350",  // 1: Manufacturer
-#else
-    "RP2040",  // 1: Manufacturer
-#endif
-    "HID Remapper XXXX",  // 2: Product
-    "123456789012",       // 3: Serial Number
+    (const char[]){ 0x09, 0x04 },   // 0: is supported language is English (0x0409)
+    "SHARKOON Technologies GmbH",   // 1: Manufacturer
+    "USB HID KM Device",            // 2: Product
+    "USB000000000000",              // 3: Serial Number
 };
 
 // Invoked when received GET DEVICE DESCRIPTOR
@@ -200,16 +196,9 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
             _desc_str[1 + i] = str[i];
         }
 
-        if (index == 2) {
-            uint64_t unique_id = get_unique_id();
-            for (uint8_t i = 0; i < 4; i++) {
-                _desc_str[1 + chr_count - 4 + i] = id_chars[(unique_id >> (15 - i * 5)) & 0x1F];
-            }
-        }
-
         if (index == 3) {
             uint64_t unique_id = get_unique_id();
-            for (uint8_t i = 0; i < 12; i++) {
+            for (uint8_t i = 3; i < 15; i++) {
                 _desc_str[1 + i] = id_chars[(unique_id >> (55 - i * 5)) & 0x1F];
             }
         }
